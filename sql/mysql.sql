6b3841e48469ac69c73a83d59a7810c9ed9d67e2
eviltomorrow <eviltomorrow@gmail.com>
2020-09-13 17:46:18 +0800

    dev

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -4,9 +4,9 @@ CREATE DATABASE `aphrodite` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 -- stock 表
 drop table if exists `aphrodite`.`stock`;
 create table `aphrodite`.`stock` (
-    `source` VARCHAR(8) NOT NULL COMMENT '来源',
-    `code` CHAR(8) NOT NULL COMMENT '股票代码',
+    `code` CHAR(8) NOT NULL COMMENT '股票代码',
     `name` VARCHAR(32) NOT NULL COMMENT '名称',
+    `source` VARCHAR(8) NOT NULL COMMENT '来源',
     `create_timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `modify_timestamp` TIMESTAMP COMMENT '修改时间',
     PRIMARY KEY(`code`)
@@ -17,7 +17,7 @@ create table `aphrodite`.`stock` (
 drop table if exists `aphrodite`.`quote_day`;
 create table `aphrodite`.`quote_day` (
     `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `code` CHAR(8) NOT NULL COMMENT '股票代码',
+    `code` CHAR(8) NOT NULL COMMENT '股票代码',
     `open` DECIMAL NOT NULL COMMENT '开盘价',
     `close` DECIMAL NOT NULL COMMENT '收盘价',
     `high` DECIMAL NOT NULL COMMENT '最高价',
@@ -34,7 +34,7 @@ create table `aphrodite`.`quote_day` (
 drop table if exists `aphrodite`.`quote_week`;
 create table `aphrodite`.`quote_week` (
     `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `code` CHAR(8) NOT NULL COMMENT '股票代码',
+    `code` CHAR(8) NOT NULL COMMENT '股票代码',
     `open` DECIMAL NOT NULL COMMENT '开盘价',
     `close` DECIMAL NOT NULL COMMENT '收盘价',
     `high` DECIMAL NOT NULL COMMENT '最高价',
